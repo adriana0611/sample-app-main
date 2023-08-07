b93e301fa7e26ab1f2f73900074f13c2ef2e82ce
adriana0611 <adriana.jardim.dev@gmail.com>
2023-08-07 02:24:30 -0400

    chore: common-module-2 page

--- src/pages/CommonModule2Page.tsx
+++ src/pages/CommonModule2Page.tsx
@@ -1,5 +1,7 @@
+import { CommonModule2 } from "adriana-sample-app-common-module-2/dist/esm/components/CommonModule2";
+
 // -----------------------------------------------------------------
 
 export const CommonModule2Page = () => {
-  return <div>CommonModule2</div>;
+  return <CommonModule2 />;
 };

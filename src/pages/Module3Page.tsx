48391bdbccfb42df9e7d0f85ec63007515634169
adriana0611 <adriana.jardim.dev@gmail.com>
2023-08-07 03:00:46 -0400

    chore: module 3 page

--- src/pages/Module3Page.tsx
+++ src/pages/Module3Page.tsx
@@ -1,5 +1,7 @@
+import { Module3 } from "adriana-sample-app-module-3/dist/esm/components/Module3";
+
 // -----------------------------------------------------------------
 
 export const Module3Page = () => {
-  return <div>Module3</div>;
+  return <Module3 />;
 };
